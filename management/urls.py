781b820d516f8c2825e8ef509215d23277ae046c
Manuel Hazael Coloma Bardales <zdelp21@gmail.com>
2020-09-14 19:08:53 -0500

    app pages added

--- management/urls.py
+++ management/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app.urls', namespace='app')),
+    # path('', include('app.urls', namespace='app')),
+    path('', include('pages.urls')),
 ]
